7e964cc2e3b5feabd8c1096ae986e8681e7af398
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-26 23:11:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist,artistName,songTitle) {
 }
 
 function removeFromPlaylist(playlist,artistName) {
-  delete playlist.artistName;
-  return playlist;
+  var playlist2 = delete playlist.artistName;
+  return playlist2
 }
 
