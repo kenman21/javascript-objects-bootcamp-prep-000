f258a935766dd90b6a596b2b0f15a1abd61c39f1
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-26 22:25:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,7 @@ function updatePlaylist(playlist,artistName,songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
-/* function updatePlaylist(playlist,artistName,songTitle) {
+function updatePlaylist(playlist,artistName,songTitle) {
   delete playlist.artistName; 
 }
 
-*/
